b4450e25a2ed9cc48af38556081c12da3cb58780
Michal Superfine <superfin@g.jct.ac.il>
2025-04-07 20:44:03 +0300

    try run server in railway

--- server/db.js
+++ server/db.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config(); 
 
+const { Pool } = pkg;
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
